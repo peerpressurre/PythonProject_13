58207ae516afd7cfffd65571170fa42c7f948cdc
“Your <yaroslavdisiak@gmail.com>
2022-10-29 14:41:59 +0300

    feature/hw_ex2

---
+++ main.py
@@ -0,0 +1,16 @@
+def pali():
+    try:
+        text = input('enter text -')
+        text_words = input('enter words -')
+        words = text_words.split()
+        for word in words:
+            if text.count(word)>0:
+                orig = word
+                new = word.title()
+
+                text = text.replace(orig, new)
+        print(text)
+
+    except Exception as ex:
+        print(f'Error: {ex}')
+pali()
