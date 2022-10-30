7c5bf2e0ec41ded618a0a3189ce4e51bb147623a
peerpressurre <gsophiay@gmail.com>
2022-10-30 12:25:33 +0200

    finall 3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def pali():
     try:
-        text = input('')
+        text = input('Enter your text->')
         res = text.count('.') + text.count('!') + text.count('?')
+        print(f'The number of sentences:{res}')
     except Exception as ex:
         print(f'Error: {ex}')
 pali()
