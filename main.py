12dc941b56092487b755f9c1b92227cbe1040cd5
peerpressurre <gsophiay@gmail.com>
2022-10-30 12:33:49 +0200

    finall 2

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ def pali():
         text = input('enter text -')
         text_words = input('enter words -')
         words = text_words.split()
+        print(words)
         for word in words:
             if text.count(word)>0:
                 orig = word
-                new = word[0].upper()+word[1:]
+                new = word.title()
 
                 text = text.replace(orig, new)
         print(text)
