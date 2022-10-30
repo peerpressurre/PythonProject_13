48657fd9d8f8ab5606479092e3187c123d4ea5f9
peerpressurre <gsophiay@gmail.com>
2022-10-30 12:34:39 +0200

    finall 2

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ def pali():
         text = input('enter text -')
         text_words = input('enter words -')
         words = text_words.split()
-        print(words)
         for word in words:
             if text.count(word)>0:
                 orig = word
